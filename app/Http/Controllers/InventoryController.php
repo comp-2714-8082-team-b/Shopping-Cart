02e6b712b7750ed2d60d42f4c8c425f5c9a498d9
Davin Deol <davindeol@gmail.com>
2019-02-26 11:09:02 -0800

    Added controller for inventory

---
+++ app/Http/Controllers/InventoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Http\Controllers;
+use App\User;
+use App\Http\Controllers\Controller;
+
+/**
+ * Description of Inventory
+ *
+ * @author DavinDeol
+ */
+class InventoryController {
+    //put your code here
+    public function inventoryPage()
+    {
+        return __METHOD__;
+    }
+}
