c9ef126cfec0d331390f42b7f279aaa9f3c5dcad
Davin Deol <davindeol@gmail.com>
2019-03-05 16:54:05 -0800

    Added 'add to cart' button

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -3,4 +3,5 @@ Get the item's column name with the following syntax:
 <open parentheses><open parentheses> $item->name <closing parentheses><closing parentheses> -->
 @foreach ($items as $item)
 <p>Item</p>
+<button class="addToCart" name="{{ $item->modelNumber }}">Add to Cart</button>
 @endforeach
