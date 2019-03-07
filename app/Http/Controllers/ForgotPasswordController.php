e444b877aff9a558b895702083888e85a1a70a7c
Davin Deol <davindeol@gmail.com>
2019-03-07 11:35:49 -0800

    Added ForgotPasswordController

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -10,43 +10,27 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB; // So that you can make MySQL statements
-use App\User as User;
+use Illuminate\Support\Facades\Hash;
 
 /**
- * Description of LoginController
+ * Description of ForgotPasswordController
  *
  * @author DavinDeol
  */
-class LoginController extends Controller {
-    
-    /**
-     * 
-     * @return login view
-     */
-    public function login()
+class ForgotPasswordController extends Controller {
+    public function forgotPassword()
     {
         $data = array();
-        $data["title"] = "Login";
-        return view('login', compact('data'));
+        $data['title'] = "Forgot Password";
+        return view('register', compact('data'));
     }
-    
-    /**
-     * This method takes the user's entered email and passwords and tests to see
-     * if they're valid. If they are then it returns the name of the user,
-     * otherwise it'll return false.
-     * 
-     * @param request -Stores the form data
-     * @returns whether or not the user was valid
-     */
-    public function submitLogin(Request $request)
+    public function submitForgotPassword(Request $request)
     {
         $validator = \Validator::make($request->all(),
             [
                 'email' => 'required',
-                'password' => 'required'
             ]
         );
-
         if (!$validator->fails()) {
             $credentials = $request->only('email', 'password');
             if (Auth::attempt($credentials, $request->input('rememberMe'))) {
@@ -56,16 +40,7 @@ class LoginController extends Controller {
                 return redirect()->route('login');
             }
         } else {
-            return redirect()->route('login');
+            return $validator->errors()->messages();
         }
-    }
-    
-    /**
-     * 
-     */
-    public function logout()
-    {
-        Auth::logout();
-        return redirect()->route('inventory');
     }
 }
