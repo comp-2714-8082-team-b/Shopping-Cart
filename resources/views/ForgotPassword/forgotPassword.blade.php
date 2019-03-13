78ef6fc0b3059b8ed759b6a80f446d979634d13f
Davin Deol <davindeol@gmail.com>
2019-03-13 15:16:29 -0700

    Put single form styles into separate file

--- resources/views/ForgotPassword/forgotPassword.blade.php
+++ resources/views/ForgotPassword/forgotPassword.blade.php
@@ -1,31 +1,6 @@
 @extends('Layout/layout')
 @section('content')
-<style type="text/css">
-    body {
-      background-color: white;
-    }
-    body > .grid {
-      height: 100%;
-    }
-    .image {
-      margin-top: -100px;
-    }
-    .column {
-      max-width: 450px;
-    }
-
-    button {
-        background: none;
-	    color: inherit;
-	    border: none;
-	    padding: 0;
-	    font: inherit;
-	    cursor: pointer;
-	    outline: inherit;
-    }
-
-</style>
-
+@include('Layout/singleFormStyle')
 <div class="ui middle aligned center aligned grid">
     <div class="column">
         <h2 class="ui teal image header">
