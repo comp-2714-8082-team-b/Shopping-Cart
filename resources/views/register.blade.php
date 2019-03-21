7712200af0adddaa73e74cf9b1f7a1c8ab5a36ce
Kenichi Okiebisu <a01056715@gmail.com>
2019-03-21 01:02:30 -0700

    changed ui for register screen

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,12 +1,181 @@
 @extends('Layout/layout')
 @section('content')
 @include('Layout/singleFormStyle')
-<div class="ui middle aligned center aligned grid">
-    <div class="column">
-        <h2 class="ui teal image header">
-            <div class="content">
-                Register
-            </div>
+<link rel="stylesheet" type="text/css" href="{{ asset('public/css/login_background.css') }}">
+<style>
+.fade-in {
+  animation: fadeIn ease 5s;
+  -webkit-animation: fadeIn ease 5s;
+  -moz-animation: fadeIn ease 5s;
+  -o-animation: fadeIn ease 5s;
+  -ms-animation: fadeIn ease 5s;
+}
+
+
+@keyframes fadeIn{
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+  }
+}
+
+@-moz-keyframes fadeIn {
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+  }
+}
+
+@-webkit-keyframes fadeIn {
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+  }
+}
+
+@-o-keyframes fadeIn {
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+  }
+}
+
+@-ms-keyframes fadeIn {
+  0% {
+    opacity:0;
+  }
+  100% {
+    opacity:1;
+  }
+}
+
+/* The style below is just for the appearance of the example div */
+
+.style {
+  text-align:center;
+  padding-top:calc(50vh - 50px);
+}
+</style>
+<section class="main">
+
+
+<div class="absolute cloud_left">
+    <ul class="inline-list">
+      <li class="cloud"></li>
+      <li class="cloud"></li>
+      <li class="cloud"></li>
+    </ul>
+</div>
+
+<div class="absolute cloud_right">
+    <ul class="inline-list">
+      <li class="cloud"></li>
+      <li class="cloud"></li>
+      <li class="cloud"></li>
+    </ul>
+</div>
+
+<span class="absolute sun"></span>
+
+
+<div class="absolute landscape left_m">
+
+    <span class="grass gl">
+        <span class="land_tree leftt-gras">
+            <ul class="inline-list">
+              <li class="t_grass"></li>
+              <li class="t_grass"></li>
+              <li class="t_grass"></li>
+            </ul>
+        </span>
+    </span>
+
+    <span class="absolute tree1"></span>
+    <span class="absolute tree2"></span>
+</div>
+
+<div class="absolute landscape max_right">
+
+    <span class="grass">
+        <span class="land_tree">
+            <ul class="inline-list">
+              <li class="t_grass"></li>
+              <li class="t_grass"></li>
+              <li class="t_grass"></li>
+            </ul>
+        </span>
+    </span>
+
+    <div class="mountain">
+        <div class="r-mountain"></div>
+        <ul class="snow inline-list">
+                <li></li>
+                <li></li>
+                <li></li>
+        </ul>
+    </div>
+
+</div>
+
+<div class="absolute boat">
+    <ul class="no-bullet">
+        <ul class="no-bullet fume">
+            <li class="fume4"></li>
+            <li class="fume3"></li>
+            <li class="fume2"></li>
+            <li class="fume1"></li>
+        </ul>
+        <li class="smokestack"></li>
+        <li class="white-body">
+            <ul class="windows inline-list">
+                <li class="circle"></li>
+                <li class="circle"></li>
+                <li class="circle"></li>
+            </ul>
+        </li>
+        <li class="boat-body"></li>
+    </ul>
+</div>
+
+<div class="absolute dark-back"></div>
+
+<div class="absolute plane">
+    <ul class="no-bullet">
+        <li class="plane-body">
+            <ul class="windows inline-list">
+                <li class="circle"></li>
+                <li class="circle"></li>
+                <li class="circle"></li>
+                <li class="circle"></li>
+                <li class="circle"></li>
+            </ul>
+        </li>
+
+        <li class="wing1"></li>
+        <li class="wing1 flipwing"></li>
+        <li class="absolute teal"></li>
+    </ul>
+</div>
+
+<div class="absolute sea">
+    <span class="wave1"></span>
+    <span class="wave2"></span>
+    <span class="wave3"></span>
+    <span class="wave4"></span>
+</div>
+
+</section>
+<div class="ui middle aligned center aligned grid style fade-in">
+    <div class="column">             
+    <h1> Register</h1>
         </h2>
         <form class="ui large form" method="POST" action="{{ route('submitRegister') }}" id="registerForm">
             @csrf
@@ -54,7 +223,7 @@
                     </div>
                 </div>
 
-                <div class="ui fluid large teal submit button">
+                <div class="ui fluid large submit button">
                     <button type="submit">
                         Submit
                     </button>
@@ -68,7 +237,7 @@
             @endif
         </form>
         <a href="{{ route('login') }}" >
-            <div class="ui button">
+            <div class="ui large button">
                 Back
             </div>
         </a>
@@ -157,5 +326,6 @@
             },
         }
     });
+    
 </script>
 @endsection
