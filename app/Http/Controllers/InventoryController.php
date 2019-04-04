d9a14c37c939f61d51d5e4334aff4324c9b4f276
Davin Deol <davindeol@gmail.com>
2019-04-04 11:19:10 -0700

    Attempt to resolve bug

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -273,7 +273,7 @@ class InventoryController extends Controller {
       $item = DB::select("SELECT DISTINCT i.modelNumber, i.itemName, i.itemPrice, i.salePrice, i.brandName, i.stockQuantity, i.description, GROUP_CONCAT(DISTINCT(c.categoryName) SEPARATOR ', ') as categories, GROUP_CONCAT(DISTINCT(p.imgUrl) SEPARATOR ', ') as pictures FROM Item i LEFT JOIN Category c ON i.modelNumber=c.modelNumber LEFT JOIN Picture p ON i.modelNumber=p.modelNumber WHERE i.modelNumber='$modelNumber'")[0];
       $item->pictures = ($item->pictures) ? explode(', ', $item->pictures) : array();
       $item->categories = ($item->categories) ? explode(', ', $item->categories) : array();
-      return view('inventory/itemDescription', compact('item'));
+      return view('Inventory/itemDescription', compact('item'));
     }
 
 
